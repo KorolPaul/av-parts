b7232c8874c67a8a97252645063118ffbf1196bf
Pavel Korol <p_korol@wargaming.net>
2021-03-11 14:14:52 +0300

    search-popup

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 const fadeElement = document.querySelector('.fade');
+const fadeMobileElement = document.querySelector('.fade__mobile');
 
-fadeElement.addEventListener('click', function() {
-    document.querySelectorAll('.active').forEach(el => el.classList.remove('active'));
-})
+function closeAllOpened() {
+    document.querySelectorAll('.opened').forEach(el => el.classList.remove('opened'));
+}
+
+fadeElement.addEventListener('click', closeAllOpened);
+fadeMobileElement.addEventListener('click', closeAllOpened);
 
 /* Popup */
 const popupToggleElements = document.querySelectorAll('.js-popup-toggle')
@@ -24,8 +28,8 @@ const additionalContentCloseElement = document.querySelector('.header_additional
 
 function toggleMainmenu(e) {
     e.preventDefault();
-    fadeElement.classList.toggle('active');
-    additionalContentElement.classList.toggle('active');
+    fadeElement.classList.toggle('opened');
+    additionalContentElement.classList.toggle('opened');
 }
 
 if (menuToggleElement) {
@@ -40,8 +44,8 @@ const catalogItemElements = document.querySelectorAll('.menu_list-link');
 if (catalogMenuToggleElement) {
     catalogMenuToggleElement.addEventListener('click', function(e) {
         e.preventDefault();
-        fadeElement.classList.toggle('active');
-        catalogMenuElement.classList.toggle('active');
+        fadeMobileElement.classList.toggle('opened');
+        catalogMenuElement.classList.toggle('opened');
     });
 
     catalogItemElements.forEach(el => el.addEventListener('click', function(e) {
@@ -88,8 +92,8 @@ document.querySelectorAll('.car-brands__compact .car-brands_title').forEach(el =
 /* toggle mobile filters */
 const filtersElement = document.querySelector('.filters');
 function toggleFilters() {
-    filtersElement.classList.toggle('active');
-    fadeElement.classList.toggle('active');
+    filtersElement.classList.toggle('opened');
+    fadeElement.classList.toggle('opened');
 }
 
 const filtersToggleElement = document.querySelector('.filters-toggle');
@@ -108,4 +112,60 @@ if (filtersToggleElement) {
     document.querySelectorAll('.filters_section-title').forEach(el => el.addEventListener('click', function (e) {
         e.target.parentElement.classList.toggle('active');
     }));
+}
+
+/* Tabs */
+const tabsButtons = document.querySelectorAll('.tabs_button');
+const tabsBlocks = document.querySelectorAll('.tabs_content');
+
+if (tabsButtons.length) {
+    function switchTab(e) {
+        e.preventDefault();
+
+        const index = e.target.dataset.tabLink;
+        tabsButtons.forEach(el => el.classList.remove('active'));
+        tabsBlocks.forEach(el => el.classList.remove('active'));
+
+        tabsButtons[index - 1].classList.add('active');
+        tabsBlocks[index - 1].classList.add('active');
+    }
+
+    tabsButtons.forEach(el => el.addEventListener('click', switchTab));
+}
+
+/* Search */
+const headerSerachInput = document.querySelector('.js-header-search');
+const searchPopup = document.querySelector('.search-popup');
+
+const searchInput = document.querySelector('.js-search-input');
+const searchContainers = document.querySelectorAll('.js-search-container');
+
+headerSerachInput.addEventListener('input', function(e) {
+    const value = e.target.value;
+
+    searchPopup.classList.toggle('opened');
+    fadeElement.classList.toggle('opened');
+    searchInput.value = value;
+    searchInput.focus();
+});
+
+if (searchInput) {
+    function highlightSerachResuts(e) {
+        const value = e.target.value;
+        const lowerCasedValue = e.target.value.toLowerCase();
+
+        searchContainers.forEach(el => {
+            const lowerCasedHTML = el.innerText.toLowerCase();
+            if (value && lowerCasedHTML.includes(lowerCasedValue)) {
+                let html = el.innerText;
+                const newValue = html.substring(lowerCasedHTML.indexOf(lowerCasedValue), lowerCasedHTML.indexOf(lowerCasedValue) + lowerCasedValue.length);
+                html = `${html.substring(0, lowerCasedHTML.indexOf(lowerCasedValue))}<span class="marker">${newValue}</span>${html.substring(lowerCasedHTML.indexOf(lowerCasedValue) + value.length)}`
+                el.innerHTML = html;
+            } else {
+                el.innerHTML = el.innerText;
+            }
+        });
+    } 
+
+    searchInput.addEventListener('input', highlightSerachResuts);
 }
